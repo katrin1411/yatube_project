880c56f639062bb68703d678014b9aac9ddca0aa
Екатерина Казакова <katrin.1411@yandex.ru>
2023-02-17 20:38:29 +0300

    vv

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     # Главная страница
-    path('', index),
+    path('', index, name = 'group_list'),
     path('group/<slug:slug>/', group_posts, name = 'group_list'),
     
 ]
